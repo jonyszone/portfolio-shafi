83cc8e92707d6aa9a45a6c5e756b271302e359ce
Shafiul Islam <jonyszone@gmail.com>
2022-12-13 13:02:46 +0600

    added app page

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Shafi Ul Islam`
   return (
     <motion.article
       initial="hidden"
